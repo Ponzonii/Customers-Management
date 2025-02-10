a05a9c8889a19f8a76767de13997bfe9082d7623
Luiz Henrique <luizgalhardolh@gmail.com>
2025-02-09 22:54:26 -0300

    update

--- Database/database.py
+++ Database/database.py
@@ -1,3 +1,6 @@
-from peewee import SqliteDatabase as Sqlite
+try:
+    from peewee import SqliteDatabase as Sqlite
+except ImportError as e:
+    print(f"Error: {e}")
 
-db = Sqlite("Your path/Name of your preference")
+db = Sqlite("customers.db")
